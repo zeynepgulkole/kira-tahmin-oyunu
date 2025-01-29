0965cf68726ed647157ee1429f0ca90781ca871e
Zeynep Gul Kole <75717829+zeynepgulkole@users.noreply.github.com>
2025-01-30 00:44:12 +0300

    Kod güncellemeleri

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,30 +1,20 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import React from "react";
 
 const ProgressBar = ({ currentHouse, totalHouses }) => {
-  const { theme } = useContext(ThemeContext);
-
   return (
     <div className="lg:w-full">
-      <div className="lg:h-2 bg-gray-200 dark:bg-gray-700 lg:rounded-3xl overflow-hidden flex">
+      <div className="lg:h-2 bg-gray-200 lg:rounded-3xl overflow-hidden flex">
         {Array.from({ length: totalHouses }).map((_, index) => (
           <div
             key={index}
             style={{
               width: `${100 / totalHouses}%`,
-              backgroundColor:
-                index < currentHouse
-                  ? theme === "dark"
-                    ? "#3b82f6" // Mavi (Dark Mode)
-                    : "#2563eb" // Daha açık mavi (Light Mode)
-                  : theme === "dark"
-                  ? "#374151" // Koyu gri (Dark Mode)
-                  : "#e5e7eb", // Açık gri (Light Mode)
+              backgroundColor: index < currentHouse ? "#3b82f6" : "#e5e7eb",
             }}
           ></div>
         ))}
       </div>
-      <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-300">
+      <p className="mt-2 text-center text-sm text-gray-600">
         Ev: {currentHouse}/{totalHouses}
       </p>
     </div>
